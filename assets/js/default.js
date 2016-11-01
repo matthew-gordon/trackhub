1fc3b7da30ebd9a391233811794fcc54909ccd8f
Matthew Gordon <laxredemption5525@gmail.com>
2016-11-01 16:29:49 -0600

    Built dynamic project card

--- assets/js/default.js
+++ assets/js/default.js
@@ -4,4 +4,27 @@ $(document).ready(function(){
   // Responsive menu
   $(".button-collapse").sideNav();
 
-});
+  // Create new project card
+      $('#projectBoard').append(
+        '<div class="col s12 m4 l4">' +
+          '<div class="card">' +
+            '<div class="card-image waves-effect waves-block waves-light">' +
+              '<img class="activator" src="assets/img/placeholder4.svg">' +
+            '</div>' +
+            '<div class="card-content">' +
+              '<span class="card-title activator grey-text text-darken-4">' +
+              'projectTitle'+
+              '<i class="material-icons right">more_vert</i></span>' +
+            '</div>' +
+            '<div class="card-reveal">' +
+              '<div class="card-title">' +
+                '<span class="card-title grey-text text-darken-4">' +
+                'projectTitle '+
+                '<i class="material-icons right">close</i></span>' +
+              '</div>' +
+            '</div>' +
+          '</div>' +
+        '</div>'
+      );
+
+}); // document.ready
