04727abc543134b1110a49c9e492a2c6c6fec206
Matthew Gordon <laxredemption5525@gmail.com>
2016-11-01 16:53:57 -0600

    Added dynamic card FAB feature, updated tooltips

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,30 +1,34 @@
 'use strict';
 $(document).ready(function(){
+var createProj = $('#createProj');
+console.log(createProj);
 
   // Responsive menu
   $(".button-collapse").sideNav();
 
   // Create new project card
-      $('#projectBoard').append(
-        '<div class="col s12 m4 l4">' +
-          '<div class="card">' +
-            '<div class="card-image waves-effect waves-block waves-light">' +
-              '<img class="activator" src="assets/img/placeholder4.svg">' +
-            '</div>' +
-            '<div class="card-content">' +
-              '<span class="card-title activator grey-text text-darken-4">' +
-              'projectTitle'+
-              '<i class="material-icons right">more_vert</i></span>' +
-            '</div>' +
-            '<div class="card-reveal">' +
-              '<div class="card-title">' +
-                '<span class="card-title grey-text text-darken-4">' +
-                'projectTitle '+
-                '<i class="material-icons right">close</i></span>' +
-              '</div>' +
-            '</div>' +
-          '</div>' +
-        '</div>'
-      );
+  createProj.click(function(){
+    $('#projectBoard').append(
+      '<div class="col s12 m4 l4">' +
+      '<div class="card">' +
+      '<div class="card-image waves-effect waves-block waves-light">' +
+      '<img class="activator" src="assets/img/placeholder4.svg">' +
+      '</div><!-- card-image -->' +
+      '<div class="card-content">' +
+      '<span class="card-title activator grey-text text-darken-4">' +
+      'projectTitle'+
+      '<i class="material-icons right">more_vert</i></span>' +
+      '</div><!-- card-content -->' +
+      '<div class="card-reveal">' +
+      '<div class="card-title">' +
+      '<span class="card-title grey-text text-darken-4">' +
+      'projectTitle '+
+      '<i class="material-icons right">close</i></span>' +
+      '</div><!-- card-title -->' +
+      '</div><!-- card-reveal -->' +
+      '</div><!-- card -->' +
+      '</div><!-- col -->'
+    );
+  });
 
 }); // document.ready
