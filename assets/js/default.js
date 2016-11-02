eb756207fab4a564908150171bf4cf15f380ec0d
Matthew Gordon <laxredemption5525@gmail.com>
2016-11-02 13:53:20 -0600

    Created login simulation, added login page, utilized localstorage

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,7 +1,7 @@
 'use strict';
 $(document).ready(function(){
 var projBoard = $('#projectBoard');
-var createProj = $('#createProj');
+var createProj = $('#createProject');
 var titleField = $('#project_title');
 var descriptionField = $('#textarea1');
 var toDoList = $('#toDo>ul');
@@ -9,6 +9,8 @@ var doingList = $('#doing>ul');
 var doneList = $('#done>ul');
 var addTask= $('#addTask');
 var removeTask = $('#removeTask');
+var user = JSON.parse(localStorage.getItem('user'));
+var contents_url;
 
 
   // Responsive menu
@@ -17,50 +19,60 @@ var removeTask = $('#removeTask');
   // Modal trigger
   $('.modal-trigger').leanModal();
 
+  // Git Hub login
+  $('#authenticate').click(function(){
+    getAuth();
+  });
+
   // Create new project card
   createProj.click(function(){
     var projectTitle = titleField.val();
     var projectDescription = descriptionField.val();
 
-    //Close project info
-    $('#modal1').closeModal();
+      //Add project to Github
+      createProject();
 
-    // Close project menu
-    $('.fixed-action-btn').closeFAB();
+      //Close project info
+      $('#modalProject').closeModal();
 
-    // Clear project form fields
-    titleField.val('');
-    descriptionField.val('');
+      // Close project menu
+      $('.fixed-action-btn').closeFAB();
 
-    // Add project card to board
-    projBoard.append(
-      '<div class="col s12 m4 l4">' +
-      '<div class="card">' +
-      '<div class="card-image waves-effect waves-block waves-light">' +
-      '<img class="activator" src="assets/img/placeholder4.svg">' +
-      '</div><!-- card-image -->' +
-      '<div class="card-content">' +
-      '<span class="card-title activator grey-text text-darken-4">' +
-      projectTitle +
-      '<i class="material-icons right">more_vert</i></span>' +
-      '</div><!-- card-content -->' +
-      '<div class="card-reveal">' +
-      '<div class="card-title">' +
-      '<span class="card-title grey-text text-darken-4">' +
-      projectTitle +
-      '<i class="material-icons right">close</i></span>' +
-      '</div>' +
-      '<div class="card-content">' +
-      '<p>' + projectDescription + '</p>' +
-      '</div><!-- card-content -->' +
-      '<div class="card-action">' +
-      '<p class="center">' +
-      '<a class="btn modal-trigger teal lighten-1" href="project.html">MANAGE PROJECT</a>' +
-      '</p>' +
-      '</div>' +
-      '</div><!-- card -->' +
-      '</div><!-- col -->'
-    );
+      // Clear project form fields
+      titleField.val('');
+      descriptionField.val('');
+
+
+      // Add project card to board
+      projBoard.append(
+        '<div class="col s12 m4 l4">' +
+        '<div class="card">' +
+        '<div class="card-image waves-effect waves-block waves-light">' +
+        '<img class="activator" src="assets/img/placeholder4.svg">' +
+        '</div><!-- card-image -->' +
+        '<div class="card-content">' +
+        '<span class="card-title activator grey-text text-darken-4">' +
+        projectTitle +
+        '<i class="material-icons right">more_vert</i></span>' +
+        '</div><!-- card-content -->' +
+        '<div class="card-reveal">' +
+        '<div class="card-title">' +
+        '<span class="card-title grey-text text-darken-4">' +
+        projectTitle +
+        '<i class="material-icons right">close</i></span>' +
+        '</div>' +
+        '<div class="card-content">' +
+        '<p>' + projectDescription + '</p>' +
+        '</div><!-- card-content -->' +
+        '<div class="card-action">' +
+        '<p class="center">' +
+        '<a class="btn modal-trigger teal lighten-1" href="project.html">MANAGE PROJECT</a>' +
+        '</p>' +
+        '</div>' +
+        '</div><!-- card -->' +
+        '</div><!-- col -->'
+      );
+
   });
 
   // Add tasks to task list
@@ -81,5 +93,45 @@ var removeTask = $('#removeTask');
       );
     // }
   });
+
+  // Github Authorization
+  function getAuth() {
+    var username = $("#username").val();
+    var password = $("#password").val();
+    var auth = username + ':' + password;
+    $.ajax({
+      url: 'https://api.github.com/authorizations',
+      type: 'POST',
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader("Authorization", "Basic " + btoa(auth));
+      },
+      data: '{"scopes":["repo", "delete_repo"],"note":"create/delete repo with ajax"}'
+    }).done(function(response) {
+      var token = response.token;
+      localStorage.setItem('user', JSON.stringify({
+        name : username,
+        authToken : token
+      }));
+      console.log(token);
+      location.href = "index.html";
+    });
+  }
+
+
+  function createProject() {
+      var repo = titleField.val();
+      $.ajax({
+        url: 'https://api.github.com/user/repos',
+        type: 'POST',
+        beforeSend: function(xhr) {
+          xhr.setRequestHeader("Authorization", "token " + user.authToken);
+        },
+        data: '{"name": ' + JSON.stringify(repo) + ',"description":"repo create from ajax test","homepage": "https://trackerApp.com","auto_init":true}'
+      }).done(function(response) {
+        console.log(response);
+        contents_url = response.contents_url;
+        console.log(contents_url);
+      });
+  }
 
 }); // document.ready
