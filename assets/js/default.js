81bab8924cd54a214f9b3d38b06b6386abec08a7
Matthew Gordon <laxredemption5525@gmail.com>
2016-11-03 15:05:11 -0600

    Lot of things

--- assets/js/default.js
+++ assets/js/default.js
@@ -2,16 +2,19 @@
 $(document).ready(function(){
 var projBoard = $('#projectBoard');
 var createProj = $('#createProject');
+var deleteProj = $('#deleteProject');
 var titleField = $('#project_title');
+var deleteTitle = $('#delete_title');
 var descriptionField = $('#textarea1');
-var toDoList = $('#toDo>ul');
-var doingList = $('#doing>ul');
-var doneList = $('#done>ul');
-var addTask= $('#addTask');
-var removeTask = $('#removeTask');
-var user = JSON.parse(localStorage.getItem('user'));
-var contents_url;
+var taskList = $('#taskList>ul');
+var addTask = $('#addTask');
+var repo, project, contents_url, data;
 
+  if(localStorage.length === 0) {
+     data = localStorage.setItem("th_data", JSON.stringify({users : []}));
+  } else {
+    popProj();
+  }
 
   // Responsive menu
   $(".button-collapse").sideNav();
@@ -66,32 +69,25 @@ var contents_url;
         '</div><!-- card-content -->' +
         '<div class="card-action">' +
         '<p class="center">' +
-        '<a class="btn modal-trigger teal lighten-1" href="project.html">MANAGE PROJECT</a>' +
+        '<a class="btn modal-trigger teal lighten-1" href="#projectTasks">MANAGE PROJECT</a>' +
         '</p>' +
         '</div>' +
         '</div><!-- card -->' +
         '</div><!-- col -->'
       );
-
+      $('.modal-trigger').leanModal();
   });
 
-  // Add tasks to task list
-  addTask.click(function(e){
-    var id = e.target;
-    console.log(id);
-    // if() {
-      toDoList.append(
-        '<li class="collection-item avatar">' +
-        '<i class="material-icons circle green">list</i>' +
-        '<span class="title">Title</span>' +
-        '<p>First Line <br>' +
-        '   Second Line' +
-        '</p>' +
-        '<a href="#!" class="secondary-content">' +
-        '<i class="material-icons">grade</i></a>' +
+  //Add tasks to task list
+  addTask.click(function(){
+      taskList.append(
+        '<li class="collection-item">' +
+        '<div>' +
+        'Alvin' +
+        '<a href="#!" class="secondary-content"><i class="material-icons">send</i></a>' +
+        '</div>' +
         '</li>'
       );
-    // }
   });
 
   // Github Authorization
@@ -108,30 +104,123 @@ var contents_url;
       data: '{"scopes":["repo", "delete_repo"],"note":"create/delete repo with ajax"}'
     }).done(function(response) {
       var token = response.token;
-      localStorage.setItem('user', JSON.stringify({
+
+      // Create user on auth
+      data = JSON.parse(localStorage.getItem('th_data'));
+      data.users.push({
         name : username,
-        authToken : token
-      }));
-      console.log(token);
+        authToken : token,
+        projects : {}
+      });
+      localStorage.setItem("th_data", JSON.stringify(data));
       location.href = "index.html";
     });
   }
 
-
+  // Create project on Github
   function createProject() {
-      var repo = titleField.val();
+      data = JSON.parse(localStorage.getItem('th_data'));
+      repo = titleField.val();
+      project = descriptionField.val();
+      repo = repo.replace(' ', '-');
+      console.log(repo);
       $.ajax({
         url: 'https://api.github.com/user/repos',
         type: 'POST',
         beforeSend: function(xhr) {
-          xhr.setRequestHeader("Authorization", "token " + user.authToken);
+          xhr.setRequestHeader("Authorization", "token " + data.users[0].authToken);
         },
-        data: '{"name": ' + JSON.stringify(repo) + ',"description":"repo create from ajax test","homepage": "https://trackerApp.com","auto_init":true}'
+        data: '{"name": ' + JSON.stringify(repo) + ',"description":' + JSON.stringify(project) + ',"homepage": "https://trackerApp.com","auto_init":true}'
       }).done(function(response) {
-        console.log(response);
         contents_url = response.contents_url;
-        console.log(contents_url);
+
+        // Create project
+        data = JSON.parse(localStorage.getItem('th_data'));
+        data.users[0].projects[repo] = {
+            name : repo,
+            desc : project
+        };
+        localStorage.setItem("th_data", JSON.stringify(data));
       });
+  }
+
+  // Delete project on Github
+  deleteProj.click(function(){
+    deleteProject();
+
+    //Close project info
+    $('#modalDelete').closeModal();
+
+    // Close project menu
+    $('.fixed-action-btn').closeFAB();
+
+  });
+
+  function deleteProject() {
+    data = JSON.parse(localStorage.getItem('th_data'));
+    var deleteProject = deleteTitle.val().replace(' ', '-');
+
+    console.log(deleteProject);
+    $.ajax({
+      url: 'https://api.github.com/repos/' + data.users[0].name + '/' + deleteProject,
+      type: 'DELETE',
+      beforeSend: function(xhr) {
+          xhr.setRequestHeader("Authorization", "token " + data.users[0].authToken);
+        }
+      }).done(function() {
+      delete data.users[0].projects[deleteProject];
+      localStorage.setItem("th_data", JSON.stringify(data));
+      projBoard.empty();
+      popProj();
+      console.log('repo was deleted!');
+    });
+  }
+
+  // Populate project
+  function popProj() {
+    data = JSON.parse(localStorage.getItem('th_data'));
+
+    // Check for users
+    if(data.users.length > 0) {
+     // check for projects
+      if(Object.keys(data.users[0].projects).length > 0) {
+        for ( var proj in data.users[0].projects ) {
+          projBoard.append(
+            '<div class="col s12 m4 l4">' +
+            '<div class="card">' +
+            '<div class="card-image waves-effect waves-block waves-light">' +
+            '<img class="activator" src="assets/img/placeholder4.svg">' +
+            '</div><!-- card-image -->' +
+            '<div class="card-content">' +
+            '<span class="card-title activator grey-text text-darken-4">' +
+            data.users[0].projects[proj].name +
+            '<i class="material-icons right">more_vert</i></span>' +
+            '</div><!-- card-content -->' +
+            '<div class="card-reveal">' +
+            '<div class="card-title">' +
+            '<span class="card-title grey-text text-darken-4">' +
+            data.users[0].projects[proj].name +
+            '<i class="material-icons right">close</i></span>' +
+            '</div>' +
+            '<div class="card-content">' +
+            '<p>' + data.users[0].projects[proj].desc + '</p>' +
+            '</div><!-- card-content -->' +
+            '<div class="card-action">' +
+            '<p class="center">' +
+            '<a class="btn modal-trigger teal lighten-1" href="#projectTasks">MANAGE PROJECT</a>' +
+            '</p>' +
+            '</div>' +
+            '</div><!-- card -->' +
+            '</div><!-- col -->'
+          );
+        }
+      } else {
+        console.log('there are no projects for this user');
+        console.log(data);
+      }
+    } else {
+      console.log('there are no users');
+    }
   }
 
 }); // document.ready
---
+++ dataModel.js
@@ -0,0 +1,23 @@
+'use strict';
+var data;
+
+// data model
+localStorage.setItem("th_data", JSON.stringify({}));
+
+// get data
+data = JSON.parse(localStorage.getItem('th_data'));
+
+// create user
+data.users.push({});
+
+//re store data
+localStorage.setItem("th_data", JSON.stringify(data));
+
+//get data
+data = JSON.parse(localStorage.getItem('th_data'));
+
+//create project
+data.users[0].projects = [];
+
+// re store data
+localStorage.setItem("th_data", JSON.stringify(data));
